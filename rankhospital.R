010b818a4e8289add7c3c30e908349b55ed4907c
Jason James <insitefuldesign@gmail.com>
2015-12-22 13:28:47 -0500

    added basic setup code from original best function

--- rankhospital.R
+++ rankhospital.R
@@ -18,6 +18,33 @@ rankhospital <- function(state, outcome, num = "best") {
     }
   }
   
+  ## subset tdata frame matching only those rows with the selected state
+  
+  selected.state <- tdata[tdata$State == state, ]
+  
+  ## select hospital name and outcome columns
+  
+  if (outcome == 'heart attack') {
+    selection <- 11
+  } else if (outcome == 'heart failure') {
+    selection <- 17
+  } else if (outcome == 'pneumonia') {
+    selection <- 23
+  }
+  
+  chosen.outcome <- selected.state[, c(2, selection)]
+  
+  ## scrub na's out of the data
+  
+  out <- complete.cases(chosen.outcome)
+  chosen.outcome <- chosen.outcome[out, ]
+  
+  ## sort the data frame based on outcome
+  
+  chosen.outcome <- chosen.outcome[with(chosen.outcome, order(chosen.outcome[[2]])), ]
+  
+  ## create a data frame with Hospital Name, State, and 30-day death rate
+  
   ## Return hospital name in that state with the given rank
   ## 30-day death rate
 }
