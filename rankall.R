d435c22ad105a60bacfb737ba2ff32da05905bea
Jason James <insitefuldesign@gmail.com>
2015-12-23 15:06:20 -0500

    added comments to the rankall function

--- rankall.R
+++ rankall.R
@@ -1,4 +1,28 @@
 rankall <- function(outcome, num = "best") {
+        #The function reads the outcome-of-care-measures.csv ﬁle and returns a 
+        #2-column data frame containing the hospital in each state that has the 
+        #ranking speciﬁed in num. For example the function call rankall("heart 
+        #attack", "best") would return a data frame containing the names of the 
+        #hospitals that are the best in their respective states for 30-day heart
+        #attack death rates. The function should return a value for every state 
+        #(some may be NA). The ﬁrst column in the data frame is named hospital, 
+        #which contains the hospital name, and the second column is named state,
+        #which contains the 2-character abbreviation for the state name. 
+        #Hospitals that do not have data on a particular outcome are excluded 
+        #from the set of hospitals when deciding the rankings. Handling ties. 
+        #The rankall function should handle ties in the 30-day mortality rates 
+        #in the same way that the rankhospital function handles ties.
+        #
+        #
+        # Args: 
+        #       outcome: must be one of three possible outcomes:  heart attack, 
+        #       pneumonia, or heart failure.
+        #
+        # Returns:
+        #
+        #       A 2-column data frame containing the names of the hospitals that
+        #       are the best in their respective states for 30-day outcome
+        #       rates. 
         
         ## Read outcome data
         
